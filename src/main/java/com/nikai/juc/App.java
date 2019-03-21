5d7fdde686c3959488803b10d15661f5ab918b9b
nikai <keni1006@163.com>
2019-03-21 22:47:14 +0800

    init project

---
+++ src/main/java/com/nikai/juc/App.java
@@ -0,0 +1,13 @@
+package com.nikai.juc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
